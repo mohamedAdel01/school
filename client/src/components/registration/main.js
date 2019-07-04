8da7db858a2217bf0c727727c0b7624ad13df71f
mohamedadel01 <mohamed7adel96@gmail.com>
2019-07-05 01:41:24 +0200

    add design for registration

--- client/src/components/registration/main.js
+++ client/src/components/registration/main.js
@@ -1,24 +1,44 @@
 import React, { Component } from 'react'
-import { Row, Col } from 'antd'
+import { Row, Col, Button } from 'antd'
 
 class Main extends Component {
   render () {
     return (
       <div id='Main'>
-        <Row gutter={6}>
+        <Row type='flex' gutter={6}>
           <Col md={{span: 24}} lg={{span: 12}}>
-          <Row type='flex' justify='center'>
+          <Row
+            className='teacher_sec'
+            type='flex'
+            justify='center'
+            align='middle'>
             <section>
               <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEeUtS8IprtrykN1eqlCG4jGOyzR_hoVtGmawe-cOwFgtRDXna' alt='teacher' />
-              <h1>teacher</h1>
+              <h1>Teacher</h1>
+              <Button className="gr" block size='large'>
+                Sign UP
+              </Button>
+              <Button block type='primary' size='large'>
+                Sign IN
+              </Button>
             </section>
           </Row>
           </Col>
           <Col md={{span: 24}} lg={{span: 12}}>
-          <Row type='flex' justify='center'>
+          <Row
+            className='student_sec'
+            type='flex'
+            justify='center'
+            align='middle'>
             <section>
               <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQboUrHs7Pn1HT11yBxInE9e9mUAT6uoDruv0ASRATk4aMs3DKUgw' alt='student' />
-              <h1>student</h1>
+              <h1>Student</h1>
+              <Button className="gr" block size='large'>
+                Sign UP
+              </Button>
+              <Button block type='primary' size='large'>
+                Sign IN
+              </Button>
             </section>
           </Row>
           </Col>
