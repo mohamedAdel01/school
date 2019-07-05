34b65bcef2f0ec9379c7c2eeb2142da9f42d42f9
mohamedadel01 <mohamed7adel96@gmail.com>
2019-07-05 18:10:20 +0200

    add teacher mutation

---
+++ server/src/graphQL/mutations/add_teacher.js
@@ -0,0 +1,39 @@
+const graphql = require('graphql')
+const {GraphQLString, GraphQLID, GraphQLList} = graphql
+
+// GRAPHQL TYPES
+const {TeacherType} = require('../types/index')
+
+// MONGODB MODELS
+const TeacherModel = require('../../models/teacher')
+
+const GradeMutation = {
+  type: TeacherType,
+  args: {
+    username: {type: GraphQLString},
+    password: {type: GraphQLString},
+    firstname: {type: GraphQLString},
+    lastname: {type: GraphQLString},
+    email: {type: GraphQLString},
+    image: {type: GraphQLString},
+    subject_id: {type: GraphQLID},
+    classrooms_ids: {type: new GraphQLList(GraphQLID)},
+    grades_ids: {type: new GraphQLList(GraphQLID)}
+  },
+  resolve(parent, args) {
+    let teacher = new TeacherModel({
+      username: args.username,
+      password: args.password,
+      firstname: args.firstname,
+      lastname: args.lastname,
+      email: args.email,
+      image: args.image,
+      subject_id: args.subject_id,
+      classrooms_ids: args.classrooms_ids,
+      grades_ids: args.grades_ids
+    })
+    return teacher.save()
+  }
+}
+
+module.exports = GradeMutation
