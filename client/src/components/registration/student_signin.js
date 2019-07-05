32d45b91013b1e65774319d64586bf197ee3768d
mohamedadel01 <mohamed7adel96@gmail.com>
2019-07-05 15:22:56 +0200

    add functunality

--- client/src/components/registration/student_signin.js
+++ client/src/components/registration/student_signin.js
@@ -11,9 +11,11 @@ class Student_Signin extends Component {
           <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQboUrHs7Pn1HT11yBxInE9e9mUAT6uoDruv0ASRATk4aMs3DKUgw' alt='student' />
           <h1>Sign IN</h1>
           <form>
-            <Input placeholder='email' />
-            <Input.Password placeholder='password' />
-            <Button type='primary' block size='large'>
+            <Input placeholder='email' onChange={e => this.handleSubmit(e, 'email')}/>
+            <p className="danger">{this.state.error.email}</p>
+            <Input.Password placeholder='password' onChange={e =>this.handleSubmit(e, 'password')}/>
+            <p className="danger">{this.state.error.password}</p>
+            <Button onClick={this.submit.bind(this)} type='primary' block size='large'>
               Sign In
             </Button>
           </form>
@@ -21,6 +23,44 @@ class Student_Signin extends Component {
         </Row>
       </div>
     )
+  }
+
+  // DATA
+  state = {
+    req: {
+      email: null,
+      password: null
+    },
+    error: false
+  }
+
+  // METHODS
+  handleSubmit(e, type) {
+    this.setState({
+      error: false,
+      req: 
+        {
+          ...this.state.req,
+          [type]: e.target.value
+        }
+      })
+  }
+
+  submit() {
+    let check = Object.keys(this.state.req).filter(key => !this.state.req[key])
+    if(check.length) {
+      check.map(key => {
+        this.setState({
+          error: 
+            {
+              ...this.state.error,
+              [key]: "please fill this input"
+            }
+          })
+      })
+      return
+    }
+    console.log(this.state)
   }
 }
 
